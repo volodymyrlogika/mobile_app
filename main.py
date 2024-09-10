534a6a625a26ef3dda930ccae4ed9ccca77d255a
volodymyrlogika <volodymyr.logika@gmail.com>
2024-09-10 18:02:14 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import kivy
+kivy.require('2.1.0') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+class MyApp(App):
+    def build(self):
+        text = Label(text="Вітаю в мобільній розробці!")
+        btn = Button(text="Не натискай!")
+        btn.on_press = self.btn_click
+        col = BoxLayout(orientation="vertical")
+        col.add_widget(text)
+        col.add_widget(btn)
+        return col
+    
+    def btn_click(self):
+        print("Я ж казав не натискатии!!!!")
+        exit()
+    
+
+
+MyApp().run()
